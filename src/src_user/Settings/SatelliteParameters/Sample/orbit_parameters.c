002f63ffe9350cd67e94edf599502e2fe2772d4c
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-02 23:44:32 +0200

    Add SatelliteParameters as first test

---
+++ src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
@@ -0,0 +1,8 @@
+/**
+ * @file  orbit_parameters.c
+ * @brief 軌道・時刻関連の衛星固有パラメータを管理する
+ */
+
+#include <Settings/SatelliteParameters/orbit_parameters.h>
+
+const double ORBIT_PARAMETERS_reference_jday = 2459932.000;  // 2022/12/18 12:00:00
---
+++ src/src_user/Settings/SatelliteParameters/orbit_parameters.h
@@ -0,0 +1,6 @@
+/**
+ * @file  orbit_parameters.h
+ * @brief 軌道・時刻関連の衛星固有パラメータを管理する
+ */
+
+extern const double ORBIT_PARAMETERS_reference_jday; //!< Reference time for orbit calculation [Julian day]
