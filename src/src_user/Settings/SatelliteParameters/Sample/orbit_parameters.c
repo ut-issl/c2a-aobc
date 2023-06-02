6dc963bdd0e41722a95c3cec07e2e7419152d82a
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-02 23:54:24 +0200

    Fix include path

--- src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
@@ -3,6 +3,6 @@
  * @brief 軌道・時刻関連の衛星固有パラメータを管理する
  */
 
-#include <Settings/SatelliteParameters/orbit_parameters.h>
+#include <src_user/Settings/SatelliteParameters/orbit_parameters.h>
 
 const double ORBIT_PARAMETERS_reference_jday = 2459932.000;  // 2022/12/18 12:00:00
