79d44261f748cd2d59433dae5093b6f8918c405c
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-03 11:18:09 +0200

    Add orbit calculation method

--- src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
@@ -5,4 +5,8 @@
 
 #include <src_user/Settings/SatelliteParameters/orbit_parameters.h>
 
+// Time
 const double ORBIT_PARAMETERS_reference_jday = 2459932.000;  // 2022/12/18 12:00:00
+
+// Orbit
+const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method = APP_ORBIT_CALC_METHOD_KEPLER;
--- src/src_user/Settings/SatelliteParameters/orbit_parameters.h
+++ src/src_user/Settings/SatelliteParameters/orbit_parameters.h
@@ -3,4 +3,10 @@
  * @brief 軌道・時刻関連の衛星固有パラメータを管理する
  */
 
+#include <src_user/Applications/UserDefined/AOCS/OrbitEstimation/orbit_calculator.h>
+
+// Time
 extern const double ORBIT_PARAMETERS_reference_jday; //!< Reference time for orbit calculation [Julian day]
+
+// Orbit
+extern const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method; //!< Orbit calculation method
