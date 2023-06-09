c8c3784b0e02c067c90a4060fb7502dca8f55d77
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-09 21:20:28 +0200

    Add attitude target parameters file

---
+++ src/src_user/Settings/SatelliteParameters/Sample/attitude_target_parameters.c
@@ -0,0 +1,9 @@
+/**
+ * @file  attitude_target_parameters.c
+ * @brief 姿勢目標の衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/attitude_target_parameters.h>
+
+// Target Calculation Mode
+const APP_TARGET_ATT_CALC_MODE ATTITUDE_TARGET_PARAMETERS_mode = APP_TARGET_ATT_CALC_MODE_MANUAL;
---
+++ src/src_user/Settings/SatelliteParameters/attitude_target_parameters.h
@@ -0,0 +1,14 @@
+/**
+ * @file  attitude_target_parameters.h
+ * @brief 姿勢目標の衛星固有パラメータを管理する
+ */
+
+#ifndef ATTITUDE_TARGET_PARAMETERS_H_
+#define ATTITUDE_TARGET_PARAMETERS_H_
+
+#include <src_user/Applications/UserDefined/AOCS/TargetAttitude/target_attitude_calculator.h>
+
+// Target Calculation Mode
+extern const APP_TARGET_ATT_CALC_MODE ATTITUDE_TARGET_PARAMETERS_mode; //!< Attitude target mode
+
+#endif // ATTITUDE_TARGET_PARAMETERS_H_
