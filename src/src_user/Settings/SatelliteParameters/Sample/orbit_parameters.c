712671dc9eb4b637ec18e7b4b5cda8d9f8ac57fd
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-09 10:45:23 +0200

    Add SatelliteParameters for Kepler orbit propagator

--- src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
@@ -4,9 +4,18 @@
  */
 
 #include <src_user/Settings/SatelliteParameters/orbit_parameters.h>
+#include <src_user/Library/physical_constants.h>
 
 // Time
 const double ORBIT_PARAMETERS_reference_jday = 2459932.000;  // 2022/12/18 12:00:00
 
 // Orbit
+// Orbit Calculator
 const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method = APP_ORBIT_CALC_METHOD_KEPLER;
+// Kepler Orbit
+const double ORBIT_PARAMETERS_kepler_semi_major_axis_km = 6899.3234f;
+const double ORBIT_PARAMETERS_kepler_eccentricity = 4.86396e-4f;
+const double ORBIT_PARAMETERS_kepler_inclination_rad = PHYSICAL_CONST_degree_to_radian(97.50372f);
+const double ORBIT_PARAMETERS_kepler_raan_rad = PHYSICAL_CONST_degree_to_radian(340.20189f);
+const double ORBIT_PARAMETERS_kepler_arg_perigee_rad = PHYSICAL_CONST_degree_to_radian(221.60537f);
+const double ORBIT_PARAMETERS_kepler_epoch_jday = 2.459931936719433e6;
--- src/src_user/Settings/SatelliteParameters/orbit_parameters.h
+++ src/src_user/Settings/SatelliteParameters/orbit_parameters.h
@@ -12,6 +12,14 @@
 extern const double ORBIT_PARAMETERS_reference_jday; //!< Reference time for orbit calculation [Julian day]
 
 // Orbit
+// Orbit Calculator
 extern const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method; //!< Orbit calculation method
+// Kepler Orbit
+extern const double ORBIT_PARAMETERS_kepler_semi_major_axis_km; //!< Semi-major axis for Kepler Propagator [km]
+extern const double ORBIT_PARAMETERS_kepler_eccentricity;       //!< Eccentricity for Kepler Propagator
+extern const double ORBIT_PARAMETERS_kepler_inclination_rad;    //!< Inclination for Kepler Propagator [rad]
+extern const double ORBIT_PARAMETERS_kepler_raan_rad;           //!< RAAN (Right Ascension of Ascending Node) for Kepler Propagator [rad]
+extern const double ORBIT_PARAMETERS_kepler_arg_perigee_rad;    //!< Argment of perigee for Kepler Propagator [rad]
+extern const double ORBIT_PARAMETERS_kepler_epoch_jday;         //!< Epoch for Kepler Propagator [Julian day]
 
 #endif // ORBIT_PARAMETERS_H_
