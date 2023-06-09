1d5d04bedcf764885c7533ba43dc25b73bdb02f7
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-09 11:47:01 +0200

    Add SatelliteParameters for GPS-R propagator

--- src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/orbit_parameters.c
@@ -20,12 +20,29 @@ const double ORBIT_PARAMETERS_kepler_raan_rad = PHYSICAL_CONST_degree_to_radian(
 const double ORBIT_PARAMETERS_kepler_arg_perigee_rad = PHYSICAL_CONST_degree_to_radian(221.60537f);
 const double ORBIT_PARAMETERS_kepler_epoch_jday = 2.459931936719433e6;
 // TLE for SGP4
+// 1st line
 const double ORBIT_PARAMETERS_tle_epoch_year = 22;
 const double ORBIT_PARAMETERS_tle_epoch_day = 352.00000000;
 const double ORBIT_PARAMETERS_tle_b_star = 0.0;
+// 2nd line
 const double ORBIT_PARAMETERS_tle_inclination_deg = 97.5068f;
 const double ORBIT_PARAMETERS_tle_raan_deg = 339.7118f;
 const double ORBIT_PARAMETERS_tle_eccentricity = 0.0011775f;
 const double ORBIT_PARAMETERS_tle_arg_perigee_deg = 245.9837f;
 const double ORBIT_PARAMETERS_tle_mean_anomaly_deg = 114.0163f;
 const double ORBIT_PARAMETERS_tle_mean_motion_rpd = 15.15782335f;
+// GPS-R Orbit Propagator
+// Initial orbital elements
+const double ORBIT_PARAMETERS_gpsr_semi_major_axis_km = 6899.3234f;
+const double ORBIT_PARAMETERS_gpsr_eccentricity = 4.86396e-4f;
+const double ORBIT_PARAMETERS_gpsr_inclination_rad = PHYSICAL_CONST_degree_to_radian(97.50372f);
+const double ORBIT_PARAMETERS_gpsr_raan_rad = PHYSICAL_CONST_degree_to_radian(340.20189f);
+const double ORBIT_PARAMETERS_gpsr_arg_perigee_rad = PHYSICAL_CONST_degree_to_radian(0.0f);
+const double ORBIT_PARAMETERS_gpsr_epoch_jday = 2.459931936719433e6;
+// Threshold
+const double ORBIT_PARAMETERS_gpsr_threshold_semi_major_axis_km = 50.0f;
+const double ORBIT_PARAMETERS_gpsr_threshold_eccentricity = 0.1f;
+const double ORBIT_PARAMETERS_gpsr_threshold_inclination_rad = PHYSICAL_CONST_degree_to_radian(20.0f);
+const double ORBIT_PARAMETERS_gpsr_threshold_raan_rad = PHYSICAL_CONST_degree_to_radian(20.0f);
+const double ORBIT_PARAMETERS_gpsr_threshold_arg_perigee_rad = PHYSICAL_CONST_degree_to_radian(50.0f);
+const double ORBIT_PARAMETERS_gpsr_threshold_epoch_jday = 0.1;
--- src/src_user/Settings/SatelliteParameters/orbit_parameters.h
+++ src/src_user/Settings/SatelliteParameters/orbit_parameters.h
@@ -22,15 +22,32 @@ extern const double ORBIT_PARAMETERS_kepler_raan_rad;           //!< RAAN (Right
 extern const double ORBIT_PARAMETERS_kepler_arg_perigee_rad;    //!< Argment of perigee for Kepler Propagator [rad]
 extern const double ORBIT_PARAMETERS_kepler_epoch_jday;         //!< Epoch for Kepler Propagator [Julian day]
 // TLE for SGP4
+// 1st line
 extern const double ORBIT_PARAMETERS_tle_epoch_year;        //!< Epoch year in TLE 1st line for SGP4 Propagator
 extern const double ORBIT_PARAMETERS_tle_epoch_day;         //!< Epoch day in TLE 1st line for SGP4 Propagator
 extern const double ORBIT_PARAMETERS_tle_b_star;            //!< B-star in TLE 1st line for SGP4 Propagator
+// 2nd line
 extern const double ORBIT_PARAMETERS_tle_inclination_deg;   //!< Inclination in TLE 2nd line for SGP4 Propagator [deg]
 extern const double ORBIT_PARAMETERS_tle_raan_deg;          //!< RAAN (Right Ascension of Ascending Node) in TLE 2nd line for SGP4 Propagator [deg]
 extern const double ORBIT_PARAMETERS_tle_eccentricity;      //!< Eccentricity in TLE 2nd line for SGP4 Propagator [-]
 extern const double ORBIT_PARAMETERS_tle_arg_perigee_deg;   //!< Argment of perigee in TLE 2nd line for SGP4 Propagator [deg]
 extern const double ORBIT_PARAMETERS_tle_mean_anomaly_deg;  //!< Mean anomaly in TLE 2nd line for SGP4 Propagator [deg]
 extern const double ORBIT_PARAMETERS_tle_mean_motion_rpd;   //!< Mean motion in TLE 2nd line for SGP4 Propagator [RPD]
+// GPS-R Orbit Propagator
+// Initial orbital elements
+extern const double ORBIT_PARAMETERS_gpsr_semi_major_axis_km; //!< Semi-major axis for GPS-R Orbit Propagator [km]
+extern const double ORBIT_PARAMETERS_gpsr_eccentricity;       //!< Eccentricity for GPS-R Orbit Propagator
+extern const double ORBIT_PARAMETERS_gpsr_inclination_rad;    //!< Inclination for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_raan_rad;           //!< RAAN (Right Ascension of Ascending Node) for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_arg_perigee_rad;    //!< Argment of perigee for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_epoch_jday;         //!< Epoch for GPS-R Orbit Propagator [Julian day]
+// Threshold
+extern const double ORBIT_PARAMETERS_gpsr_threshold_semi_major_axis_km; //!< Semi-major axis threshold for GPS-R Orbit Propagator [km]
+extern const double ORBIT_PARAMETERS_gpsr_threshold_eccentricity;       //!< Eccentricity threshold for GPS-R Orbit Propagator
+extern const double ORBIT_PARAMETERS_gpsr_threshold_inclination_rad;    //!< Inclination threshold for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_threshold_raan_rad;           //!< RAAN (Right Ascension of Ascending Node) threshold for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_threshold_arg_perigee_rad;    //!< Argment of perigee threshold for GPS-R Orbit Propagator [rad]
+extern const double ORBIT_PARAMETERS_gpsr_threshold_epoch_jday;         //!< Epoch threshold for GPS-R Orbit Propagator [Julian day]
 
 
 #endif // ORBIT_PARAMETERS_H_
