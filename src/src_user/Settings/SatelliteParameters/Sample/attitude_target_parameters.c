699de0f0a64fa46dfdb6aea23a01e4181b9fffe6
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-10 09:07:52 +0200

    Add target from orbit

--- src/src_user/Settings/SatelliteParameters/Sample/attitude_target_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/attitude_target_parameters.c
@@ -11,3 +11,17 @@ const APP_TARGET_ATT_CALC_MODE ATTITUDE_TARGET_PARAMETERS_mode = APP_TARGET_ATT_
 // Manual mode target
 const Quaternion ATTITUDE_TARGET_PARAMETERS_quaternion_target_i2t = {0.5f, 0.5f, 0.5f, 0.5f};
 
+// Target calculation from orbit
+// Main target
+extern const APP_TAFO_TARGET_DIRECITON ATTITUDE_TARGET_PARAMETERS_main_target_direction = APP_TAFO_TARGET_DIRECITON_SUN;
+extern const float ATTITUDE_TARGET_PARAMETERS_vec_to_main_target_body[PHYSICAL_CONST_THREE_DIM] = {1.0f, 0.0f, 0.0f};
+// Sub target
+extern const APP_TAFO_TARGET_DIRECITON ATTITUDE_TARGET_PARAMETERS_sub_target_direction = APP_TAFO_TARGET_DIRECITON_EARTH_CENTER;
+extern const float ATTITUDE_TARGET_PARAMETERS_vec_to_sub_target_body[PHYSICAL_CONST_THREE_DIM] = {0.0f, 1.0f, 0.0f};
+// Offset angle
+extern const MATRIX33_ROTATION_AXIS ATTITUDE_TARGET_PARAMETERS_offset_angle_axis = MATRIX33_ROTATION_AXIS_X;
+extern const float ATTITUDE_TARGET_PARAMETERS_offset_angle_rad = 0.0f;
+// Target on earth surface
+extern const float ATTITUDE_TARGET_PARAMETERS_target_lla_rad_m[PHYSICAL_CONST_THREE_DIM] = { PHYSICAL_CONST_degree_to_radian(35.7130f),
+                                                                                             PHYSICAL_CONST_degree_to_radian(139.7596f),
+                                                                                             23.0f};
