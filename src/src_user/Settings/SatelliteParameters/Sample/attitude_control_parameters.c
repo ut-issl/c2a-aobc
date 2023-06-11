c6fdd8b6092b804bfa2658b70ef55d5fdd5e30cf
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-11 16:02:49 +0200

    Add attitude control parameter files

---
+++ src/src_user/Settings/SatelliteParameters/Sample/attitude_control_parameters.c
@@ -0,0 +1,7 @@
+/**
+ * @file  attitude_control_parameters.c
+ * @brief 姿勢制御関連の衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/attitude_control_parameters.h>
+
---
+++ src/src_user/Settings/SatelliteParameters/attitude_control_parameters.h
@@ -0,0 +1,9 @@
+/**
+ * @file  attitude_control_parameters.h
+ * @brief 姿勢制御関連の衛星固有パラメータを管理する
+ */
+
+#ifndef ATTITUDE_CONTROL_PARAMETERS_H_
+#define ATTITUDE_CONTROL_PARAMETERS_H_
+
+#endif // ATTITUDE_CONTROL_PARAMETERS_H_
