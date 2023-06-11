66d2184e22ef502cb30a561151da363d14c7a67d
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-11 16:20:54 +0200

    Add Bdot parameters

--- src/src_user/Settings/SatelliteParameters/Sample/attitude_control_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/attitude_control_parameters.c
@@ -5,3 +5,7 @@
 
 #include <src_user/Settings/SatelliteParameters/attitude_control_parameters.h>
 
+// Bdor
+const float ATTITUDE_CONTROL_PARAMETERS_bdot_control_gain[PHYSICAL_CONST_THREE_DIM] = { -0.1f, -0.1f, -0.1f };
+const uint32_t ATTITUDE_CONTROL_PARAMETERS_bdot_minimum_time_derivative_step_ms = 100;
+const uint32_t ATTITUDE_CONTROL_PARAMETERS_bdot_mtq_output_time_length_ms = 1000;
--- src/src_user/Settings/SatelliteParameters/attitude_control_parameters.h
+++ src/src_user/Settings/SatelliteParameters/attitude_control_parameters.h
@@ -6,4 +6,12 @@
 #ifndef ATTITUDE_CONTROL_PARAMETERS_H_
 #define ATTITUDE_CONTROL_PARAMETERS_H_
 
+#include <stdint.h>
+#include <src_user/Library/physical_constants.h>
+
+// Bdot
+extern const float ATTITUDE_CONTROL_PARAMETERS_bdot_control_gain[PHYSICAL_CONST_THREE_DIM]; //!< Control gain for B-dot
+extern const uint32_t ATTITUDE_CONTROL_PARAMETERS_bdot_minimum_time_derivative_step_ms;     //!< Minimum interval for magnetic field derivative [ms]
+extern const uint32_t ATTITUDE_CONTROL_PARAMETERS_bdot_mtq_output_time_length_ms;           //!< Keep time for MTQ ON as an actuation [ms]
+
 #endif // ATTITUDE_CONTROL_PARAMETERS_H_
