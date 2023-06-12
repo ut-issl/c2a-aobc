e7c0c6b4a0ed0a09ef272bf7b3fb605db77dacfe
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-12 21:29:11 +0200

    Add mpu parameter file

---
+++ src/src_user/Settings/SatelliteParameters/Sample/mpu9250_parameters.c
@@ -0,0 +1,7 @@
+/**
+ * @file  mpu9250_parameters.c
+ * @brief MPU9250に関する衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/mpu9250_parameters.h>
+
---
+++ src/src_user/Settings/SatelliteParameters/mpu9250_parameters.h
@@ -0,0 +1,9 @@
+/**
+ * @file  mpu9250_parameters.h
+ * @brief MPU9250に関する衛星固有パラメータを管理する
+ */
+
+#ifndef MPU9250_PARAMETERS_H_
+#define MPU9250_PARAMETERS_H_
+
+#endif // MPU9250_PARAMETERS_H_
