51aecb9bb972483e09beeaded5863a839cfd6fde
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-13 20:39:46 +0200

    Add temperature calibration parameters

--- src/src_user/Settings/SatelliteParameters/Sample/mpu9250_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/mpu9250_parameters.c
@@ -5,3 +5,18 @@
 
 #include <src_user/Settings/SatelliteParameters/mpu9250_parameters.h>
 
+// Communication port
+
+// Magnetometer bias
+const float MPU9250_PARAMETERS_mag_bias_compo_nT[PHYSICAL_CONST_THREE_DIM] = { -18417.53f, -30423.31f, 20969.18f };
+
+// Gyro Bias and scale factor temperature calibration
+const float MPU9250_PARAMETERS_temperature_range_high_degC = 60.0f;
+const float MPU9250_PARAMETERS_temperature_range_low_degC = -50.0f;
+
+const float MPU9250_PARAMETERS_bias_coeff_compo_x[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { -0.024f, 0.0002f };
+const float MPU9250_PARAMETERS_scale_factor_coeff_compo_x[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { 1.0f, 0.0f};
+const float MPU9250_PARAMETERS_bias_coeff_compo_y[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { 0.0058f, -0.0005f };
+const float MPU9250_PARAMETERS_scale_factor_coeff_compo_y[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { 1.0f, 0.0f};
+const float MPU9250_PARAMETERS_bias_coeff_compo_z[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { 0.0179f, 0.0001f };
+const float MPU9250_PARAMETERS_scale_factor_coeff_compo_z[MPU9250_PARAMETERS_kNumCoeffTempCalib] = { 1.0f, 0.0f};
--- src/src_user/Settings/SatelliteParameters/mpu9250_parameters.h
+++ src/src_user/Settings/SatelliteParameters/mpu9250_parameters.h
@@ -6,4 +6,28 @@
 #ifndef MPU9250_PARAMETERS_H_
 #define MPU9250_PARAMETERS_H_
 
+#include <stdint.h>
+#include <src_user/Applications/DriverInstances/di_mpu9250.h>
+#include <src_user/Library/physical_constants.h>
+
+// Constant value
+const uint8_t MPU9250_PARAMETERS_kNumCoeffTempCalib = 2; //!< Number of coefficient for calibration. This value cannot be changed since it relates Tlm/Cmd definition.
+
+// Communication port
+
+// Magnetometer bias
+extern const float MPU9250_PARAMETERS_mag_bias_compo_nT[PHYSICAL_CONST_THREE_DIM]; //!< Magnetic field observation bias @ component frame [nT]
+
+
+// Gyro Bias and scale factor temperature calibration
+extern const float MPU9250_PARAMETERS_temperature_range_high_degC;  //!< Temperature range high [degC]
+extern const float MPU9250_PARAMETERS_temperature_range_low_degC;   //!< Temperature range low [degC]
+
+extern const float MPU9250_PARAMETERS_bias_coeff_compo_x[MPU9250_PARAMETERS_kNumCoeffTempCalib];          //!< Bias coefficients @ component frame X axis
+extern const float MPU9250_PARAMETERS_scale_factor_coeff_compo_x[MPU9250_PARAMETERS_kNumCoeffTempCalib];  //!< Scale Factor coefficients @ component frame X axis
+extern const float MPU9250_PARAMETERS_bias_coeff_compo_y[MPU9250_PARAMETERS_kNumCoeffTempCalib];          //!< Bias coefficients @ component frame Y axis
+extern const float MPU9250_PARAMETERS_scale_factor_coeff_compo_y[MPU9250_PARAMETERS_kNumCoeffTempCalib];  //!< Scale Factor coefficients @ component frame Y axis
+extern const float MPU9250_PARAMETERS_bias_coeff_compo_z[MPU9250_PARAMETERS_kNumCoeffTempCalib];          //!< Bias coefficients @ component frame Z axis
+extern const float MPU9250_PARAMETERS_scale_factor_coeff_compo_z[MPU9250_PARAMETERS_kNumCoeffTempCalib];  //!< Scale Factor coefficients @ component frame Z axis
+
 #endif // MPU9250_PARAMETERS_H_
