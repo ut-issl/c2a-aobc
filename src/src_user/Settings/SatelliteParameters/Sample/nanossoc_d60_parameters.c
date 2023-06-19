0e231d5781d95ff1ca562371bb459b261fd29bb9
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-19 22:12:26 +0200

    Add frame conversion parameters

--- src/src_user/Settings/SatelliteParameters/Sample/nanossoc_d60_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/nanossoc_d60_parameters.c
@@ -4,3 +4,9 @@
  */
 
 #include <src_user/Settings/SatelliteParameters/nanossoc_d60_parameters.h>
+
+// Frame conversion
+const Quaternion NANOSSOC_D60_PARAMETERS_py_quaternion_c2b = { 0.70710665f, 0.0f, 0.0f, 0.70710665f };
+const Quaternion NANOSSOC_D60_PARAMETERS_my_quaternion_c2b = { 0.0f, -0.70710665f, 0.70710665f, 0.0f };
+const Quaternion NANOSSOC_D60_PARAMETERS_pz_quaternion_c2b = { 0.0f, 0.0f, 0.70710665f, 0.70710665f };
+const Quaternion NANOSSOC_D60_PARAMETERS_mz_quaternion_c2b = { -0.707106471f, 0.707106471f, 0.0f, 0.0f };
--- src/src_user/Settings/SatelliteParameters/nanossoc_d60_parameters.h
+++ src/src_user/Settings/SatelliteParameters/nanossoc_d60_parameters.h
@@ -6,6 +6,13 @@
 #ifndef NANOSSOC_D60_PARAMETERS_H_
 #define NANOSSOC_D60_PARAMETERS_H_
 
+#include <src_user/Library/quaternion.h>
+
+// Frame conversion
+extern const Quaternion NANOSSOC_D60_PARAMETERS_py_quaternion_c2b;  //!< Frame conversion quaternion for PY sun sensor
+extern const Quaternion NANOSSOC_D60_PARAMETERS_my_quaternion_c2b;  //!< Frame conversion quaternion for MY sun sensor
+extern const Quaternion NANOSSOC_D60_PARAMETERS_pz_quaternion_c2b;  //!< Frame conversion quaternion for PZ sun sensor
+extern const Quaternion NANOSSOC_D60_PARAMETERS_mz_quaternion_c2b;  //!< Frame conversion quaternion for MZ sun sensor
 
 
 #endif // NANOSSOC_D60_PARAMETERS_H_
