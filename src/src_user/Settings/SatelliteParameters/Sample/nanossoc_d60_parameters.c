d8d318f8cc2905602a7feb61de682b972fca8254
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-19 21:11:51 +0200

    Add parameter files

---
+++ src/src_user/Settings/SatelliteParameters/Sample/nanossoc_d60_parameters.c
@@ -0,0 +1,6 @@
+/**
+ * @file  nanossoc_d60_parameters.c
+ * @brief nanossoc-D60に関する衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/nanossoc_d60_parameters.h>
---
+++ src/src_user/Settings/SatelliteParameters/nanossoc_d60_parameters.h
@@ -0,0 +1,11 @@
+/**
+ * @file  nanossoc_d60_parameters.h
+ * @brief nanossoc-D60に関する衛星固有パラメータを管理する
+ */
+
+#ifndef NANOSSOC_D60_PARAMETERS_H_
+#define NANOSSOC_D60_PARAMETERS_H_
+
+
+
+#endif // NANOSSOC_D60_PARAMETERS_H_
