ff9717e1ed24bfb813a4a4e0cb250412b8d82588
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-21 21:11:26 +0200

    Add FDIR application parameters

--- src/src_user/Settings/SatelliteParameters/Sample/fdir_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/fdir_parameters.c
@@ -4,3 +4,44 @@
  */
 
 #include <src_user/Settings/SatelliteParameters/fdir_parameters.h>
+
+// Mode Manager
+const float FDIR_PARAMETERS_ang_vel_conv_limit_rad_s = PHYSICAL_CONST_degree_to_radian(0.5f);
+const float FDIR_PARAMETERS_ang_vel_conv_time_limit_s = 5.0f * 60.0f;
+
+const float FDIR_PARAMETERS_ang_vel_norm_increase_limit_rad_s = PHYSICAL_CONST_degree_to_radian(0.5f);
+const float FDIR_PARAMETERS_ang_vel_anomaly_detection_period_s = 200.0f;
+
+const float FDIR_PARAMETERS_sun_angle_div_limit_rad = PHYSICAL_CONST_degree_to_radian(45.0f);
+const float FDIR_PARAMETERS_sun_angle_div_time_limit_s = 50.0f * 60.0f;
+
+const float FDIR_PARAMETERS_three_axis_div_limit_rad = PHYSICAL_CONST_degree_to_radian(30.0f);
+const float FDIR_PARAMETERS_three_axis_div_time_limit_s = 5.0f * 60.0f;
+
+const float FDIR_PARAMETERS_sun_invisible_time_limit_s = 50.0f * 60.0f;
+const float FDIR_PARAMETERS_stt_invisible_time_limit_s = 10.0f * 60.0f;
+
+// S/W over current detection
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_pic_mA = 200;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_stim210_mA = 1000;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_sagitta_mA = 500;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_oem7600_mA = 1000;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rm3100_mA = 200;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_nanossoc_d60_mA = 150;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_mtq_mA = 2000;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_x_mA = 2000;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_y_mA = 2000;
+const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_z_mA = 2000;
+
+// H/W over current detection
+// If the measured voltage is smaller than the following values, we assume that the H/W OC protection was executed by the INA260.
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_pic_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_stim210_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_sagitta_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_oem7600_V = 1.0f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_rm3100_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_nanossoc_d60_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_mtq_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_x_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_y_V = 0.5f;
+const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_z_V = 0.5f;
--- src/src_user/Settings/SatelliteParameters/fdir_parameters.h
+++ src/src_user/Settings/SatelliteParameters/fdir_parameters.h
@@ -7,5 +7,51 @@
 #define FDIR_PARAMETERS_H_
 
 #include <src_user/Library/physical_constants.h>
+#include <stdint.h>
+
+// Mode Manager
+extern const float FDIR_PARAMETERS_ang_vel_conv_limit_rad_s;    //!< Angular velocity convergence detection limit [rad/s]
+extern const float FDIR_PARAMETERS_ang_vel_conv_time_limit_s;   //!< Angular velocity convergence detection time limit [s]
+
+extern const float FDIR_PARAMETERS_ang_vel_norm_increase_limit_rad_s;   //!< Angular velocity increase anomaly detection limit [rad/s]
+extern const float FDIR_PARAMETERS_ang_vel_anomaly_detection_period_s;  //!< Angular velocity anomaly detection period [s]
+
+extern const float FDIR_PARAMETERS_sun_angle_div_limit_rad;     //!< Sun pointing divergence detection limit [rad]
+extern const float FDIR_PARAMETERS_sun_angle_div_time_limit_s;  //!< Sun pointing divergence detection time limit [s]
+
+extern const float FDIR_PARAMETERS_three_axis_div_limit_rad;    //!< Three axis control divergence detection limit [rad]
+extern const float FDIR_PARAMETERS_three_axis_div_time_limit_s; //!< Three axis control divergence detection time limit [s]
+
+extern const float FDIR_PARAMETERS_sun_invisible_time_limit_s;  //!< Sun invisible anomaly detection time limit [s]
+extern const float FDIR_PARAMETERS_stt_invisible_time_limit_s;  //!< STT invisible anomaly detection time limit [s]
+
+// S/W over current detection
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_pic_mA;          //!< S/W over current detection threshold for PIC [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_stim210_mA;      //!< S/W over current detection threshold for STIM210 [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_sagitta_mA;      //!< S/W over current detection threshold for SAGITTA [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_oem7600_mA;      //!< S/W over current detection threshold for OEM7600 [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rm3100_mA;       //!< S/W over current detection threshold for RM3100 [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_nanossoc_d60_mA; //!< S/W over current detection threshold for nanoSSOC D60 [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_mtq_mA;          //!< S/W over current detection threshold for MTQ [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_x_mA;     //!< S/W over current detection threshold for RW003 X-axis [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_y_mA;     //!< S/W over current detection threshold for RW003 Y-axis [mA]
+extern const uint16_t FDIR_PARAMETERS_sw_oc_threshold_rw0003_z_mA;     //!< S/W over current detection threshold for RW003 Z-axis [mA]
+
+// H/W over current detection
+// If the measured voltage is smaller than the following values, we assume that the H/W OC protection was executed by the INA260.
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_pic_V;          //!< H/W over current detection threshold for PIC [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_stim210_V;      //!< H/W over current detection threshold for STIM210 [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_sagitta_V;      //!< H/W over current detection threshold for SAGITTA [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_oem7600_V;      //!< H/W over current detection threshold for OEM7600 [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_rm3100_V;       //!< H/W over current detection threshold for RM3100 [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_nanossoc_d60_V; //!< H/W over current detection threshold for nanoSSOC D60 [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_mtq_V;          //!< H/W over current detection threshold for MTQ [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_x_V;     //!< H/W over current detection threshold for RW003 X-axis [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_y_V;     //!< H/W over current detection threshold for RW003 Y-axis [V]
+extern const float FDIR_PARAMETERS_hw_oc_detection_threshold_rw0003_z_V;     //!< H/W over current detection threshold for RW003 Z-axis [V]
+
+
+// Temperature anomaly
+// TBI
 
 #endif // FDIR_PARAMETERS_H_
