443ff3fe6836781325c83d520162d61967951949
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-21 20:25:10 +0200

    Add FDIR parameter files

---
+++ src/src_user/Settings/SatelliteParameters/Sample/fdir_parameters.c
@@ -0,0 +1,6 @@
+/**
+ * @file  fdir_parameters.c
+ * @brief FDIRに関する衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/fdir_parameters.h>
---
+++ src/src_user/Settings/SatelliteParameters/fdir_parameters.h
@@ -0,0 +1,11 @@
+/**
+ * @file  structure_parameters.h
+ * @brief 衛星構造に関する衛星固有パラメータを管理する
+ */
+
+#ifndef FDIR_PARAMETERS_H_
+#define FDIR_PARAMETERS_H_
+
+#include <src_user/Library/physical_constants.h>
+
+#endif // FDIR_PARAMETERS_H_
