29db489f154c62da31528e4104b41f52b167f72e
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-26 19:17:04 +0200

    Add INA260 parameters files

---
+++ src/src_user/Settings/SatelliteParameters/Sample/ina260_parameters.c
@@ -0,0 +1,6 @@
+/**
+ * @file  ina260_parameters.c
+ * @brief INA260に関する衛星固有パラメータを管理する
+ */
+
+#include <src_user/Settings/SatelliteParameters/ina260_parameters.h>
---
+++ src/src_user/Settings/SatelliteParameters/ina260_parameters.h
@@ -0,0 +1,10 @@
+/**
+ * @file  ina260_parameters.h
+ * @brief INA260に関する衛星固有パラメータを管理する
+ */
+
+#ifndef INA260_PARAMETERS_H_
+#define INA260_PARAMETERS_H_
+
+
+#endif // INA260_PARAMETERS_H_
