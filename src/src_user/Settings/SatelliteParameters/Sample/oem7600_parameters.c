99d103e77c0d8c1beeef7f7a29fd91d9ba42db6d
Toshihiro Suzuki <suzuki@arkedgespace.com>
2023-06-27 19:24:23 +0900

    [Fix] Fix OEM7600 spike filter parameter into array

--- src/src_user/Settings/SatelliteParameters/Sample/oem7600_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/oem7600_parameters.c
@@ -6,14 +6,14 @@
 #include <src_user/Settings/SatelliteParameters/oem7600_parameters.h>
 
 // Spike Filter
-uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_accept = 3;
-uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_reject_continued_warning = 60;
-double OEM7600_PARAMETERS_position_spike_filter_config_reject_threshold_m = 10000.0;
-double OEM7600_PARAMETERS_position_spike_filter_config_amplitude_limit_to_accept_as_step_m = 8000.0;
+uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_accept[PHYSICAL_CONST_THREE_DIM] = {3, 3, 3};
+uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_reject_continued_warning[PHYSICAL_CONST_THREE_DIM] = {60, 60, 60};
+double OEM7600_PARAMETERS_position_spike_filter_config_reject_threshold_m[PHYSICAL_CONST_THREE_DIM] = {10000.0, 10000.0, 10000.0};
+double OEM7600_PARAMETERS_position_spike_filter_config_amplitude_limit_to_accept_as_step_m[PHYSICAL_CONST_THREE_DIM] = {8000.0, 8000.0, 8000.0};
 
-uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_accept = 3;
-uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_reject_continued_warning = 60;
-double OEM7600_PARAMETERS_velocity_spike_filter_config_reject_threshold_m_s = 1000.0;
-double OEM7600_PARAMETERS_velocity_spike_filter_config_amplitude_limit_to_accept_as_step_m_s = 100.0;
+uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_accept[PHYSICAL_CONST_THREE_DIM] = {3, 3, 3};
+uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_reject_continued_warning[PHYSICAL_CONST_THREE_DIM] = {60, 60, 60};
+double OEM7600_PARAMETERS_velocity_spike_filter_config_reject_threshold_m_s[PHYSICAL_CONST_THREE_DIM] = {1000.0, 1000.0, 1000.0};
+double OEM7600_PARAMETERS_velocity_spike_filter_config_amplitude_limit_to_accept_as_step_m_s[PHYSICAL_CONST_THREE_DIM] = {100.0, 100.0, 100.0};
 
 // Sun intensity threshold
--- src/src_user/Settings/SatelliteParameters/oem7600_parameters.h
+++ src/src_user/Settings/SatelliteParameters/oem7600_parameters.h
@@ -9,14 +9,14 @@
 #include <src_user/Library/quaternion.h>
 
 // Spike Filter
-extern uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_accept;                   //!< Count limit to accept about position
-extern uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_reject_continued_warning; //!< Count limit to reject continued warning about position
-extern double OEM7600_PARAMETERS_position_spike_filter_config_reject_threshold_m;                       //!< Reject threshold about position [m]
-extern double OEM7600_PARAMETERS_position_spike_filter_config_amplitude_limit_to_accept_as_step_m;      //!< Amplitude limit to accept as step input about position [m]
+extern uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_accept[PHYSICAL_CONST_THREE_DIM];                   //!< Count limit to accept about position
+extern uint8_t OEM7600_PARAMETERS_position_spike_filter_config_count_limit_to_reject_continued_warning[PHYSICAL_CONST_THREE_DIM]; //!< Count limit to reject continued warning about position
+extern double OEM7600_PARAMETERS_position_spike_filter_config_reject_threshold_m[PHYSICAL_CONST_THREE_DIM];                       //!< Reject threshold about position [m]
+extern double OEM7600_PARAMETERS_position_spike_filter_config_amplitude_limit_to_accept_as_step_m[PHYSICAL_CONST_THREE_DIM];      //!< Amplitude limit to accept as step input about position [m]
 
-extern uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_accept;                   //!< Count limit to accept about velocity
-extern uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_reject_continued_warning; //!< Count limit to reject continued warning about velocity
-extern double OEM7600_PARAMETERS_velocity_spike_filter_config_reject_threshold_m_s;                     //!< Reject threshold about velocity [m/s]
-extern double OEM7600_PARAMETERS_velocity_spike_filter_config_amplitude_limit_to_accept_as_step_m_s;    //!< Amplitude limit to accept as step input about velocity [m/s]
+extern uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_accept[PHYSICAL_CONST_THREE_DIM];                   //!< Count limit to accept about velocity
+extern uint8_t OEM7600_PARAMETERS_velocity_spike_filter_config_count_limit_to_reject_continued_warning[PHYSICAL_CONST_THREE_DIM]; //!< Count limit to reject continued warning about velocity
+extern double OEM7600_PARAMETERS_velocity_spike_filter_config_reject_threshold_m_s[PHYSICAL_CONST_THREE_DIM];                     //!< Reject threshold about velocity [m/s]
+extern double OEM7600_PARAMETERS_velocity_spike_filter_config_amplitude_limit_to_accept_as_step_m_s[PHYSICAL_CONST_THREE_DIM];    //!< Amplitude limit to accept as step input about velocity [m/s]
 
 #endif // OEM7600_PARAMETERS_H_
