1399bd2db8fb4fdc891e4ea5a333ed1e82ef7878
Toshihiro Suzuki <suzuki@arkedgespace.com>
2023-06-28 17:57:43 +0900

    [Feature] Separate parameters about sensor selector

---
+++ src/src_user/Settings/SatelliteParameters/Sample/component_selector_parameters.c
@@ -0,0 +1,9 @@
+/**
+ * @file  component_selector_parameters.c
+ * @brief コンポセレクタに関する衛星固有パラメータを管理する
+ */
+
+#include "../component_selector_parameters.h"
+
+const APP_MAG_SELECTOR_STATE COMPONENT_SELECTOR_PARAMETERS_initial_selected_magnetometer = APP_MAG_SELECTOR_STATE_RM_EXT;
+const APP_GYRO_SELECTOR_STATE COMPONENT_SELECTOR_PARAMETERS_initial_selected_gyro = APP_GYRO_SELECTOR_STATE_MPU;
