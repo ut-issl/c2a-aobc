e95ab4c808f5d96ea875db70391ed0e295ac5f46
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-30 22:29:09 +0200

    Fix with user-defined repository construction test

--- src/src_user/Settings/SatelliteParameters/Sample/component_selector_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/component_selector_parameters.c
@@ -3,6 +3,6 @@
  * @brief コンポセレクタに関する衛星固有パラメータを管理する
  */
 
-#include "../component_selector_parameters.h"
+#include <src_user/Settings/SatelliteParameters/component_selector_parameters.h>
 
 const APP_MAG_SELECTOR_STATE COMPONENT_SELECTOR_PARAMETERS_initial_selected_magnetometer = APP_MAG_SELECTOR_STATE_RM_EXT;
