802949911652ab6f76c79499a0ed8352bd8a903b
satoshi_ikari <ikari@space.t.u-tokyo.ac.jp>
2023-08-03 18:41:45 +0900

    Remove setting.json reflecting a review

--- src/src_user/Test/utils/c2a_enum_utils.py
+++ src/src_user/Test/utils/c2a_enum_utils.py
@@ -6,15 +6,7 @@ import c2aenum as c2a
 
 
 def get_c2a_enum():
-    c2a_src_abs_path = os.environ.get("C2A_SRC_ABS_PATH")
-
-    # このifは環境変数が設定されているならsettings.jsonを無視したいという意図
-    if c2a_src_abs_path is None:
-        with open(os.path.dirname(__file__).replace("\\", "/") + "/../settings.json") as f:
-            json_dict = json.load(f)
-        c2a_src_abs_path = (
-            os.path.dirname(__file__).replace("\\", "/") + "/../" + json_dict["c2a_src_rel_path"]
-        )
+    c2a_src_abs_path = os.path.dirname(__file__).replace("\\", "/") + "/../../.."
 
     return c2a.load_enum(c2a_src_abs_path, "utf-8")
 
