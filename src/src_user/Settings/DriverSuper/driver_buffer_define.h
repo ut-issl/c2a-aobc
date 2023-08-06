3539b8df195748ac03ee84cbc4365f38a524ddac
Riki Nakamura <riki.nakamura.edu@gmail.com>
2023-08-06 10:42:26 +0900

    Fix typo

--- src/src_user/Settings/DriverSuper/driver_buffer_define.h
+++ src/src_user/Settings/DriverSuper/driver_buffer_define.h
@@ -9,6 +9,6 @@
 
 #define DS_STREAM_REC_BUFFER_SIZE_DEFAULT  (DS_IF_RX_BUFFER_SIZE * 2)  /*!< DS_StreamRecBuffer のバッファサイズのデフォルト値
                                                                             UART などの非同期通信はメモリに余力があれば DS_IF_RX_BUFFER_SIZE * 2 を推奨
-                                                                            詳細は dirver_super.c の @note を参照 */
+                                                                            詳細は driver_super.c の @note を参照 */
 
 #endif
