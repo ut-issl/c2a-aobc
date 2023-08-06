54f9112d70f03f2533c00d580fbd0b7530b9147d
Riki Nakamura <riki.nakamura.edu@gmail.com>
2023-08-06 14:03:16 +0900

    Resize DS_STREAM_REC_BUFFER_SIZE_I2C

--- src/src_user/Settings/DriverSuper/driver_buffer_define.h
+++ src/src_user/Settings/DriverSuper/driver_buffer_define.h
@@ -10,7 +10,7 @@
 #define DS_STREAM_REC_BUFFER_SIZE_DEFAULT  (DS_IF_RX_BUFFER_SIZE * 2)  /*!< DS_StreamRecBuffer のバッファサイズのデフォルト値
                                                                             UART などの非同期通信はメモリに余力があれば DS_IF_RX_BUFFER_SIZE * 2 を推奨
                                                                             詳細は driver_super.c の @note を参照 */
-#define DS_STREAM_REC_BUFFER_SIZE_I2C      (16 * 2)                    /*!< DS_StreamRecBuffer のバッファサイズのI2C用
+#define DS_STREAM_REC_BUFFER_SIZE_I2C      (16)                        /*!< DS_StreamRecBuffer のバッファサイズのI2C用
                                                                             I2Cのやり取りでは、数バイトのlengthなので、余裕分含む */
 
 #endif
