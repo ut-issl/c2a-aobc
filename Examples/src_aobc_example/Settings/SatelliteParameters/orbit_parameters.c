7f09d6cef94e71e8a14709af734860b9bd1094f4
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-08-09 23:27:05 +0200

    Fix default orbit

--- Examples/src_aobc_example/Settings/SatelliteParameters/orbit_parameters.c
+++ Examples/src_aobc_example/Settings/SatelliteParameters/orbit_parameters.c
@@ -11,7 +11,7 @@
 const double ORBIT_PARAMETERS_reference_jday = 2459932.000;  // 2022/12/18 12:00:00
 
 // Orbit Calculator
-const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method = APP_ORBIT_CALC_METHOD_KEPLER;
+const APP_ORBIT_CALC_METHOD ORBIT_PARAMETERS_orbit_calculation_method = APP_ORBIT_CALC_METHOD_SGP4;
 
 // Kepler Orbit
 const float ORBIT_PARAMETERS_kepler_semi_major_axis_km = 6899.3234f;
@@ -23,16 +23,16 @@ const double ORBIT_PARAMETERS_kepler_epoch_jday = 2.459931936719433e6;
 
 // TLE for SGP4
 // 1st line
-const uint8_t ORBIT_PARAMETERS_tle_epoch_year = 22;
-const double ORBIT_PARAMETERS_tle_epoch_day = 352.00000000;
+const uint8_t ORBIT_PARAMETERS_tle_epoch_year = 20;
+const double ORBIT_PARAMETERS_tle_epoch_day = 76.51604214;
 const float ORBIT_PARAMETERS_tle_b_star = 0.0;
 // 2nd line
-const float ORBIT_PARAMETERS_tle_inclination_deg = 97.5068f;
-const float ORBIT_PARAMETERS_tle_raan_deg = 339.7118f;
-const float ORBIT_PARAMETERS_tle_eccentricity = 0.0011775f;
-const float ORBIT_PARAMETERS_tle_arg_perigee_deg = 245.9837f;
-const float ORBIT_PARAMETERS_tle_mean_anomaly_deg = 114.0163f;
-const float ORBIT_PARAMETERS_tle_mean_motion_rpd = 15.15782335f;
+const float ORBIT_PARAMETERS_tle_inclination_deg = 51.6412f;
+const float ORBIT_PARAMETERS_tle_raan_deg = 86.9962f;
+const float ORBIT_PARAMETERS_tle_eccentricity = 0.0006063f;
+const float ORBIT_PARAMETERS_tle_arg_perigee_deg = 30.9353f;
+const float ORBIT_PARAMETERS_tle_mean_anomaly_deg = 329.2153f;
+const float ORBIT_PARAMETERS_tle_mean_motion_rpd = 15.49228202f;
 
 // GPS-R Orbit Propagator
 // Initial orbital elements
