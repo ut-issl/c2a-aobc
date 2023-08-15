de247996e7646160988e28d68dbe6591391a2ea0
chutaro <35984595+chutaro@users.noreply.github.com>
2023-08-15 09:56:16 +0900

    replace generate tlm in cyclic hk bc

--- src/src_user/TlmCmd/NormalBlockCommandDefinition/nbc_start_hk_tlm.c
+++ src/src_user/TlmCmd/NormalBlockCommandDefinition/nbc_start_hk_tlm.c
@@ -12,22 +12,16 @@
 void BCL_load_start_hk_tlm()
 {
   // HK General
-  BCL_tool_prepare_param_uint8(0x40);
   BCL_tool_prepare_param_uint8(Tlm_CODE_AOBC_HK_GEN);
-  BCL_tool_prepare_param_uint8(1);
-  BCL_tool_register_cmd       (1, Cmd_CODE_GENERATE_TLM);
+  BCL_tool_register_cmd       (1, Cmd_CODE_TG_GENERATE_RT_TLM);
 
   // HK Components
-  BCL_tool_prepare_param_uint8(0x40);
   BCL_tool_prepare_param_uint8(Tlm_CODE_AOBC_HK_COMPO);
-  BCL_tool_prepare_param_uint8(1);
-  BCL_tool_register_cmd       (2, Cmd_CODE_GENERATE_TLM);
+  BCL_tool_register_cmd       (2, Cmd_CODE_TG_GENERATE_RT_TLM);
 
   // HK Algorithm
-  BCL_tool_prepare_param_uint8(0x40);
   BCL_tool_prepare_param_uint8(Tlm_CODE_AOBC_HK_ALGO);
-  BCL_tool_prepare_param_uint8(1);
-  BCL_tool_register_cmd       (4, Cmd_CODE_GENERATE_TLM);
+  BCL_tool_register_cmd       (4, Cmd_CODE_TG_GENERATE_RT_TLM);
 
   BCL_tool_register_deploy (OBCT_sec2cycle(1), BC_HK_CYCLIC_TLM, TLCD_ID_DEPLOY_TLM);
 }
