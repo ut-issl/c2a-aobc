dab59f60a84b6291e55f14660e7a293366fb447b
SeirenDoi <tomofumi.doi@seiren.com>
2023-10-03 09:22:47 +0900

    Modify indent and character code

--- src/src_user/Applications/DriverInstances/di_cubewheel.c
+++ src/src_user/Applications/DriverInstances/di_cubewheel.c
@@ -1,7 +1,7 @@
 #pragma section REPRO
 /**
 * @file   di_cubewheel.c
-* @brief  CUBEWHEELÇÃDriver Instance
+* @brief  CUBEWHEEL„ÅÆDriver Instance
 */
 
 #include "di_cubewheel.h"
@@ -19,9 +19,9 @@
 
 
 static CUBEWHEEL_Driver cubewheel_driver_[CUBEWHEEL_IDX_MAX];
-const  CUBEWHEEL_Driver* const cubewheel_driver[CUBEWHEEL_IDX_MAX] = {&cubewheel_driver_[CUBEWHEEL_IDX_ON_X],
+const  CUBEWHEEL_Driver* const cubewheel_driver[CUBEWHEEL_IDX_MAX] = { &cubewheel_driver_[CUBEWHEEL_IDX_ON_X],
                                                                       &cubewheel_driver_[CUBEWHEEL_IDX_ON_Y],
-                                                                      &cubewheel_driver_[CUBEWHEEL_IDX_ON_Z]};
+                                                                      &cubewheel_driver_[CUBEWHEEL_IDX_ON_Z] };
 
 static float DI_CUBEWHEEL_rw_speed_rad_s_[CUBEWHEEL_IDX_MAX] = { 0.0f, 0.0f, 0.0f };
 static uint8_t DI_CUBEWHEEL_is_initialized_[3] = { 0, 0, 0 };  //!< 0 = not initialized, 1 = initialized
@@ -31,8 +31,8 @@ static void DI_CUBEWHEEL_init_(void);
 static void DI_CUBEWHEEL_update_(void);
 
 /**
- * @brief  AOCS MANAGERÇ…ï˚å¸çsóÒÇÉZÉbÉgÇ∑ÇÈ
- * @return AOCS_MANAGER_ERRORÇ…èÄãí
+ * @brief  AOCS MANAGER„Å´ÊñπÂêëË°åÂàó„Çí„Çª„ÉÉ„Éà„Åô„Çã
+ * @return AOCS_MANAGER_ERROR„Å´Ê∫ñÊã†
  */
 static AOCS_MANAGER_ERROR DI_CUBEWHEEL_set_direction_matrix_to_aocs_manager_(void);
 
@@ -48,7 +48,7 @@ static void DI_CUBEWHEEL_init_(void)
   CUBEWHEEL_init(&cubewheel_driver_[CUBEWHEEL_IDX_ON_Y], PORT_CH_I2C_2, I2C_DEVICE_ADDR_RW_Y, PORT_CH_GPIO_OUT_RWY_EN, RW_AXIS_ID_Y);
   CUBEWHEEL_init(&cubewheel_driver_[CUBEWHEEL_IDX_ON_Z], PORT_CH_I2C_2, I2C_DEVICE_ADDR_RW_Z, PORT_CH_GPIO_OUT_RWZ_EN, RW_AXIS_ID_Z);
 
-  // âÒì]ï˚å¸ê›íË
+  // ÂõûËª¢ÊñπÂêëË®≠ÂÆö
   float direction_x[] = { -1.0f, 0.0f, 0.0f };
   CUBEWHEEL_set_rotation_direction_b(&cubewheel_driver_[CUBEWHEEL_IDX_ON_X], direction_x);
   float direction_y[] = { 0.0f, -1.0f, 0.0f };
@@ -65,7 +65,7 @@ static void DI_CUBEWHEEL_update_(void)
 {
   uint8_t i;
 
-  // ìdåπOFFÇÃèÍçáÅAäpë¨ìxÇÕ0Ç…ê›íËÇ∑ÇÈ
+  // ÈõªÊ∫êOFF„ÅÆÂ†¥Âêà„ÄÅËßíÈÄüÂ∫¶„ÅØ0„Å´Ë®≠ÂÆö„Åô„Çã
   if (power_switch_control->switch_state_unreg[APP_PSC_UNREG_IDX_RW] == APP_PSC_STATE_OFF)
   {
     for (i = 0; i < CUBEWHEEL_IDX_MAX; i++)
@@ -94,11 +94,11 @@ static void DI_CUBEWHEEL_update_(void)
       }
       AOCS_MANAGER_set_rw_angular_velocity_rad_s(DI_CUBEWHEEL_rw_speed_rad_s_);
 
-      // ÉAÉmÉ}Éäèàóù
+      // „Ç¢„Éé„Éû„É™Âá¶ÁêÜ
       APP_ANOMALY_IDX idx = (APP_ANOMALY_IDX)(APP_ANOMALY_IDX_COMM_RWX + i);
       if (APP_ANOMALY_SET_LAST_RESULT(idx, (DS_CMD_ERR_CODE)last_result) == APP_ANOMALY_STATE_TO_ON)
       {
-        //Printf("to initial by RW(%d) comm error.\n", i);Appì‡ÇÃPrintÇÕîÇØÇÈ
+        //Printf("to initial by RW(%d) comm error.\n", i);AppÂÜÖ„ÅÆPrint„ÅØÈÅø„Åë„Çã
         APP_ANOMALY_TRANSITION_TO_INITIAL();
       }
     }
@@ -116,28 +116,28 @@ static AOCS_MANAGER_ERROR DI_CUBEWHEEL_set_direction_matrix_to_aocs_manager_(voi
   return ret;
 }
 
-CCP_EXEC_STS Cmd_DI_CUBEWHEEL_Enable(const CTCP *packet)
+CCP_EXEC_STS Cmd_DI_CUBEWHEEL_Enable(const CTCP* packet)
 {
   DS_CMD_ERR_CODE ret;
 
-  // TODO: èàóùÇèëÇ≠
-	for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
-	{
-		ret = CUBEWHEEL_Enable(&cubewheel_driver_[idx]);
-		if (ret == DS_CMD_OK)
-		{
-          DI_CUBEWHEEL_is_enabled_[idx] = 1;
-		}
-	}
+  // TODO: Âá¶ÁêÜ„ÇíÊõ∏„Åè
+  for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
+  {
+    ret = CUBEWHEEL_Enable(&cubewheel_driver_[idx]);
+    if (ret == DS_CMD_OK)
+    {
+      DI_CUBEWHEEL_is_enabled_[idx] = 1;
+    }
+  }
 
   return DS_conv_cmd_err_to_ccp_exec_sts(ret);
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_Init(const CTCP* packet)
 {
-	DS_CMD_ERR_CODE ret;
+  DS_CMD_ERR_CODE ret;
 
-	// TODO: èàóùÇèëÇ≠
+  // TODO: Âá¶ÁêÜ„ÇíÊõ∏„Åè
   for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
   {
     if (DI_CUBEWHEEL_is_enabled_[idx] == 1)
@@ -156,73 +156,73 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_Init(const CTCP* packet)
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_EndProcess(const CTCP* packet)
 {
-	DS_CMD_ERR_CODE ret;
+  DS_CMD_ERR_CODE ret;
 
-	// TODO: èàóùÇèëÇ≠
-	for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
-	{
-	  ret = CUBEWHEEL_Disable(&cubewheel_driver_[idx]);
-      DI_CUBEWHEEL_is_enabled_[idx] = 0;
-	}
+  // TODO: Âá¶ÁêÜ„ÇíÊõ∏„Åè
+  for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
+  {
+    ret = CUBEWHEEL_Disable(&cubewheel_driver_[idx]);
+    DI_CUBEWHEEL_is_enabled_[idx] = 0;
+  }
 
-	return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return DS_conv_cmd_err_to_ccp_exec_sts(ret);
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetSpeed(const CTCP* packet)
 {
-	const uint8_t* param = CCP_get_param_head(packet);
-	CUBEWHEEL_IDX cubewheel_idx;
-	uint8_t raw_val[4] = {0,0,0,0};
-	float f_speed = 0.0f;
+  const uint8_t* param = CCP_get_param_head(packet);
+  CUBEWHEEL_IDX cubewheel_idx;
+  uint8_t raw_val[4] = { 0,0,0,0 };
+  float f_speed = 0.0f;
 
-	cubewheel_idx = (CUBEWHEEL_IDX)param[0];
-	if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
-	{
-		return CCP_EXEC_ILLEGAL_PARAMETER;
-	}
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
+  {
+    return CCP_EXEC_ILLEGAL_PARAMETER;
+  }
 
-	raw_val[0] = param[1];
-	raw_val[1] = param[2];
+  raw_val[0] = param[1];
+  raw_val[1] = param[2];
 
   int16_t int16_val = 0;
   *(((unsigned char*)&int16_val) + 0) = raw_val[1];
   *(((unsigned char*)&int16_val) + 1) = raw_val[0];
 
-	int16_t speed = int16_val;
+  int16_t speed = int16_val;
 
-	CUBEWHEEL_SetReferenceSpeed(&cubewheel_driver_[cubewheel_idx],speed);
+  CUBEWHEEL_SetReferenceSpeed(&cubewheel_driver_[cubewheel_idx], speed);
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetDutyCycle(const CTCP* packet)
 {
-	const uint8_t* param = CCP_get_param_head(packet);
-	CUBEWHEEL_IDX cubewheel_idx;
-	uint8_t raw_val[4] = { 0,0,0,0 };
-	float f_duty = 0.0f;
+  const uint8_t* param = CCP_get_param_head(packet);
+  CUBEWHEEL_IDX cubewheel_idx;
+  uint8_t raw_val[4] = { 0,0,0,0 };
+  float f_duty = 0.0f;
 
-	cubewheel_idx = (CUBEWHEEL_IDX)param[0];
-	if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
-	{
-		return CCP_EXEC_ILLEGAL_PARAMETER;
-	}
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
+  {
+    return CCP_EXEC_ILLEGAL_PARAMETER;
+  }
 
-	raw_val[0] = param[1];
-	raw_val[1] = param[2];
+  raw_val[0] = param[1];
+  raw_val[1] = param[2];
 
   int16_t int16_val = 0;
   *(((unsigned char*)&int16_val) + 0) = raw_val[1];
   *(((unsigned char*)&int16_val) + 1) = raw_val[0];
 
-	int16_t duty = int16_val;
+  int16_t duty = int16_val;
 
-	CUBEWHEEL_SetDutyCycle(&cubewheel_driver_[cubewheel_idx], duty);
+  CUBEWHEEL_SetDutyCycle(&cubewheel_driver_[cubewheel_idx], duty);
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetControlMode(const CTCP* packet)
@@ -230,7 +230,7 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetControlMode(const CTCP* packet)
   const uint8_t* param = CCP_get_param_head(packet);
   CUBEWHEEL_IDX cubewheel_idx;
 
-   cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
   if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
   {
     return CCP_EXEC_ILLEGAL_PARAMETER;
@@ -255,10 +255,10 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetPwmGain(const CTCP* packet)
     return CCP_EXEC_ILLEGAL_PARAMETER;
   }
 
-  int16_t Ki= (int16_t)(param[1] << 8 | param[2]);
+  int16_t Ki = (int16_t)(param[1] << 8 | param[2]);
   uint8_t KiMultiplier = param[3];
 
-  CUBEWHEEL_SetPwmGain(&cubewheel_driver_[cubewheel_idx], Ki,KiMultiplier);
+  CUBEWHEEL_SetPwmGain(&cubewheel_driver_[cubewheel_idx], Ki, KiMultiplier);
 
   //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
   return CCP_EXEC_SUCCESS;
@@ -268,7 +268,7 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetMainGain(const CTCP* packet)
 {
   const uint8_t* param = CCP_get_param_head(packet);
   CUBEWHEEL_IDX cubewheel_idx;
-  
+
   cubewheel_idx = (CUBEWHEEL_IDX)param[0];
   if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
   {
@@ -293,76 +293,76 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetMainGain(const CTCP* packet)
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_GetStatus(const CTCP* packet)
 {
-	DS_CMD_ERR_CODE ret;
+  DS_CMD_ERR_CODE ret;
 
-	// TODO: èàóùÇèëÇ≠
-	for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
-	{
-		ret = CUBEWHEEL_GetStatus(&cubewheel_driver_[idx]);
-	}
+  // TODO: Âá¶ÁêÜ„ÇíÊõ∏„Åè
+  for (int idx = 0; idx < CUBEWHEEL_IDX_MAX; idx++)
+  {
+    ret = CUBEWHEEL_GetStatus(&cubewheel_driver_[idx]);
+  }
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SetBackupmode(const CTCP* packet)
 {
-	const uint8_t* param = CCP_get_param_head(packet);
-	CUBEWHEEL_IDX cubewheel_idx;
-	uint8_t enable_status = 0;
+  const uint8_t* param = CCP_get_param_head(packet);
+  CUBEWHEEL_IDX cubewheel_idx;
+  uint8_t enable_status = 0;
 
-	cubewheel_idx = (CUBEWHEEL_IDX)param[0];
-	if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
-	{
-		return CCP_EXEC_ILLEGAL_PARAMETER;
-	}
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
+  {
+    return CCP_EXEC_ILLEGAL_PARAMETER;
+  }
 
-	enable_status = param[1];
+  enable_status = param[1];
 
-	CUBEWHEEL_SetBackupmode(&cubewheel_driver_[cubewheel_idx], enable_status);
+  CUBEWHEEL_SetBackupmode(&cubewheel_driver_[cubewheel_idx], enable_status);
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_EnableEncoder(const CTCP* packet)
 {
-	const uint8_t* param = CCP_get_param_head(packet);
-	CUBEWHEEL_IDX cubewheel_idx;
-	uint8_t enable_status = 0;
+  const uint8_t* param = CCP_get_param_head(packet);
+  CUBEWHEEL_IDX cubewheel_idx;
+  uint8_t enable_status = 0;
 
-	cubewheel_idx = (CUBEWHEEL_IDX)param[0];
-	if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
-	{
-		return CCP_EXEC_ILLEGAL_PARAMETER;
-	}
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
+  {
+    return CCP_EXEC_ILLEGAL_PARAMETER;
+  }
 
-	enable_status = param[1];
+  enable_status = param[1];
 
-	CUBEWHEEL_EnableEncoder(&cubewheel_driver_[cubewheel_idx], enable_status);
+  CUBEWHEEL_EnableEncoder(&cubewheel_driver_[cubewheel_idx], enable_status);
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_EnableHallsensor(const CTCP* packet)
 {
-	const uint8_t* param = CCP_get_param_head(packet);
-	CUBEWHEEL_IDX cubewheel_idx;
-	uint8_t enable_status = 0;
+  const uint8_t* param = CCP_get_param_head(packet);
+  CUBEWHEEL_IDX cubewheel_idx;
+  uint8_t enable_status = 0;
 
-	cubewheel_idx = (CUBEWHEEL_IDX)param[0];
-	if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
-	{
-		return CCP_EXEC_ILLEGAL_PARAMETER;
-	}
+  cubewheel_idx = (CUBEWHEEL_IDX)param[0];
+  if (cubewheel_idx > CUBEWHEEL_IDX_MAX)
+  {
+    return CCP_EXEC_ILLEGAL_PARAMETER;
+  }
 
-	enable_status = param[1];
+  enable_status = param[1];
 
-	CUBEWHEEL_EnableHallsensor(&cubewheel_driver_[cubewheel_idx], enable_status);
+  CUBEWHEEL_EnableHallsensor(&cubewheel_driver_[cubewheel_idx], enable_status);
 
-	//return DS_conv_cmd_err_to_ccp_exec_sts(ret);
-	return CCP_EXEC_SUCCESS;
+  //return DS_conv_cmd_err_to_ccp_exec_sts(ret);
+  return CCP_EXEC_SUCCESS;
 }
 
 CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SET_ROTATION_DIRECTION_VECTOR(const CTCP* packet)
@@ -375,7 +375,7 @@ CCP_EXEC_STS Cmd_DI_CUBEWHEEL_SET_ROTATION_DIRECTION_VECTOR(const CTCP* packet)
   for (int body_axis = 0; body_axis < PHYSICAL_CONST_THREE_DIM; body_axis++)
   {
     endian_memcpy(&rotation_direction_b[body_axis], param + 1 + body_axis * sizeof(float), sizeof(float));
-  }  
+  }
 
   C2A_MATH_ERROR ret_math = CUBEWHEEL_set_rotation_direction_b(&cubewheel_driver_[idx], rotation_direction_b);
   if (ret_math != C2A_MATH_ERROR_OK) return CCP_EXEC_ILLEGAL_PARAMETER;
@@ -396,7 +396,7 @@ int DI_CUBEWHEEL_set_torque_Nm(CUBEWHEEL_IDX idx, float torque_rw_Nm)
   CUBEWHEEL_IDX cubewheel_idx = idx;
 
   float max_torque_rw_Nm = (float)(cubewheel_driver_[cubewheel_idx].info.max_torque / 1000.0);
-  f_duty = (float)(torque_rw_Nm /max_torque_rw_Nm);
+  f_duty = (float)(torque_rw_Nm / max_torque_rw_Nm);
 
   duty = (int)(f_duty * 100);
 
@@ -418,8 +418,8 @@ int DI_CUBEWHEEL_set_speed_rpm(CUBEWHEEL_IDX idx, float speed_rw_rpm)
 {
   if (idx >= CUBEWHEEL_IDX_MAX) return AOCS_ERROR_RANGE_OVER;
   if (DI_CUBEWHEEL_is_initialized_[idx] != 1) return AOCS_ERROR_OTHERS;
-  
-  // speedêßå¿
+
+  // speedÂà∂Èôê
   float max_rpm = 8000;
   float min_rpm = -max_rpm;
   if (max_rpm < speed_rw_rpm)
