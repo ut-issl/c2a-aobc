af9aec877ad7cbee65728f6fdc97ca6fe478fac9
Toshihiro Suzuki <suzuki@arkedgespace.com>
2023-11-06 17:26:18 +0900

    [MTQ] Remove internal timer and pwm handler in MTQ driver for #16

--- src/src_user/Drivers/Aocs/mtq_seiren.c
+++ src/src_user/Drivers/Aocs/mtq_seiren.c
@@ -9,13 +9,6 @@
 #include  <src_core/System/TimeManager/time_manager.h>
 #include <src_user/Library/vector3.h>
 
-
-static uint32_t MTQ_SEIREN_pwm_period_ms_ = 1000; // TODO_L: 今はコマンドで自由に変更が効くようになっているが、const化するかどうか検討が必要
-const uint32_t* MTQ_SEIREN_pwm_period_ms = &MTQ_SEIREN_pwm_period_ms_;
-
-const static uint8_t MTQ_SEIREN_kUnsignedDutyMax = 100;
-
-static void MTQ_SEIREN_update_internal_pwm_timer_(MTQ_SEIREN_Driver* mtq_seiren_driver);
 static GPIO_ERR_CODE MTQ_SEIREN_output_turn_on_(MTQ_SEIREN_Driver* mtq_seiren_driver);
 static GPIO_ERR_CODE MTQ_SEIREN_output_turn_off_(MTQ_SEIREN_Driver* mtq_seiren_driver);
 
@@ -32,84 +25,14 @@ GPIO_ERR_CODE MTQ_SEIREN_init(MTQ_SEIREN_Driver* mtq_seiren_driver, uint8_t ch_g
 
   ret = (GPIO_ERR_CODE)MTQ_SEIREN_output_turn_off_(mtq_seiren_driver);
 
-  mtq_seiren_driver->driver.pwm_info.unsigned_duty_percent = 0;
   mtq_seiren_driver->driver.polarity                       = MTQ_SEIREN_NO_OUTPUT;
   mtq_seiren_driver->driver.max_mag_moment                 = max_mag_moment;
-  mtq_seiren_driver->info.pwm_signed_duty_percent          = 0;
-  mtq_seiren_driver->driver.pwm_info.last_set_time         = TMGR_get_master_clock();
-  mtq_seiren_driver->driver.pwm_info.internal_timer_ms     = 950;
-  mtq_seiren_driver->driver.polarity_buffer                = MTQ_SEIREN_NO_OUTPUT;
-  mtq_seiren_driver->driver.unsigned_duty_percent_buffer   = 0;
 
   mtq_seiren_driver->info.magnetic_moment_direction_b[0] = 1.0f;
   mtq_seiren_driver->info.magnetic_moment_direction_b[1] = 0.0f;
   mtq_seiren_driver->info.magnetic_moment_direction_b[2] = 0.0f;
 
   return ret;
-}
-
-DS_CMD_ERR_CODE MTQ_SEIREN_set_pwm_signed_duty(MTQ_SEIREN_Driver* mtq_seiren_driver, int8_t pwm_signed_duty_percent)
-{
-  mtq_seiren_driver->info.pwm_signed_duty_percent = pwm_signed_duty_percent;
-
-  // PWM制御で使いやすいように、符号付きdutyを極性と符号なしdutyに分離して格納する
-  // PWMの途中で設定が書き換わることを避けるため，ここではCMDバッファ値のみ書換，PWMタイマのゼロにてpwm_infoに反映する
-  if (pwm_signed_duty_percent > 0)
-  {
-    mtq_seiren_driver->driver.polarity_buffer = MTQ_SEIREN_POLARITY_POSITIVE;
-    mtq_seiren_driver->driver.unsigned_duty_percent_buffer = (uint8_t)pwm_signed_duty_percent;
-  }
-  else if (pwm_signed_duty_percent < 0)
-  {
-    mtq_seiren_driver->driver.polarity_buffer = MTQ_SEIREN_POLARITY_NEGATIVE;
-    mtq_seiren_driver->driver.unsigned_duty_percent_buffer = (uint8_t)(-1.0f * pwm_signed_duty_percent);
-  }
-  else
-  {
-    mtq_seiren_driver->driver.polarity_buffer = MTQ_SEIREN_NO_OUTPUT;
-    mtq_seiren_driver->driver.unsigned_duty_percent_buffer = 0;
-  }
-
-  return DS_CMD_OK;
-}
-
-DS_CMD_ERR_CODE MTQ_SEIREN_set_pwm_period_ms(uint32_t pwm_period_ms)
-{
-  MTQ_SEIREN_pwm_period_ms_ = pwm_period_ms;
-  return DS_CMD_OK;
-}
-
-DS_CMD_ERR_CODE MTQ_SEIREN_output_pwm(MTQ_SEIREN_Driver* mtq_seiren_driver)
-{
-  GPIO_ERR_CODE ret;
-
-  MTQ_SEIREN_update_internal_pwm_timer_(mtq_seiren_driver);
-
-  // PWMの途中で設定が書き換わることを避けるため，PWMタイマのゼロでのみCMD値を反映する
-  if (mtq_seiren_driver->driver.pwm_info.internal_timer_ms == 0)
-  {
-    mtq_seiren_driver->driver.pwm_info.unsigned_duty_percent =
-    mtq_seiren_driver->driver.unsigned_duty_percent_buffer;
-
-    mtq_seiren_driver->driver.polarity = mtq_seiren_driver->driver.polarity_buffer;
-  }
-
-  uint32_t pwm_pulse_width_ms = (uint32_t)(MTQ_SEIREN_pwm_period_ms_ *
-                                           mtq_seiren_driver->driver.pwm_info.unsigned_duty_percent /
-                                           MTQ_SEIREN_kUnsignedDutyMax);
-
-  if (mtq_seiren_driver->driver.pwm_info.internal_timer_ms < pwm_pulse_width_ms)
-  {
-    ret = MTQ_SEIREN_output_turn_on_(mtq_seiren_driver);
-    if (ret != GPIO_OK) return DS_CMD_DRIVER_SUPER_ERR;
-  }
-  else
-  {
-    ret = MTQ_SEIREN_output_turn_off_(mtq_seiren_driver);
-    if (ret != GPIO_OK) return DS_CMD_DRIVER_SUPER_ERR;
-  }
-
-  return DS_CMD_OK;
 }
 
 C2A_MATH_ERROR MTQ_SEIREN_set_magnetic_moment_direction_b(MTQ_SEIREN_Driver* mtq_seiren_driver,
@@ -120,28 +43,6 @@ C2A_MATH_ERROR MTQ_SEIREN_set_magnetic_moment_direction_b(MTQ_SEIREN_Driver* mtq
   VECTOR3_copy(mtq_seiren_driver->info.magnetic_moment_direction_b,
               magnetic_moment_direction_b);
   return C2A_MATH_ERROR_OK;
-}
-
-
-/**
- * @brief  MTQ_SEIREN PWMタイマアップデート
- *
- *         MTQ_SEIREN_Driver構造体の内部のPWMタイマを進める．PWMの1周期を超えたら，タイマをリセットする．
- * @param  *mtq_seiren   : MTQ_SEIREN_Driver構造体へのポインタ
- */
-static void MTQ_SEIREN_update_internal_pwm_timer_(MTQ_SEIREN_Driver* mtq_seiren_driver)
-{
-  ObcTime current_obc_time = TMGR_get_master_clock();
-  uint32_t step_time_ms = OBCT_diff_in_msec(&(mtq_seiren_driver->driver.pwm_info.last_set_time), &current_obc_time);
-  mtq_seiren_driver->driver.pwm_info.last_set_time = current_obc_time;
-  // TODO_L : step_time_msが必ず正であることを保証する。アプリの実行周期と違いすぎる場合のエラー処理を行う。
-
-  mtq_seiren_driver->driver.pwm_info.internal_timer_ms += step_time_ms;
-
-  if (mtq_seiren_driver->driver.pwm_info.internal_timer_ms >= MTQ_SEIREN_pwm_period_ms_)
-  {
-    mtq_seiren_driver->driver.pwm_info.internal_timer_ms = 0;
-  }
 }
 
 /**
--- src/src_user/Drivers/Aocs/mtq_seiren.h
+++ src/src_user/Drivers/Aocs/mtq_seiren.h
@@ -30,8 +30,6 @@ typedef enum
  */
 typedef struct
 {
-  int8_t pwm_signed_duty_percent;                              //!< unsigned dutyとpolarityの積で表される，符号付きduty．
-  uint32_t pwm_period_ms;                                      //!< PWMの1周期の長さ[ms]
   float magnetic_moment_direction_b[PHYSICAL_CONST_THREE_DIM]; //!< 機体座標系での磁気モーメント方向単位ベクトル
 } MTQ_SEIREN_Info;
 
@@ -41,27 +39,15 @@ typedef struct
  */
 typedef struct
 {
-  // GPIOをどう扱うかは要議論
   struct
   {
     uint8_t ch_gpio_positive;
     uint8_t ch_gpio_negative;
     MTQ_SEIREN_POLARITY polarity;         //!< MTQ極性
     float max_mag_moment;                 //!< 出力可能な最大磁気モーメント
-    uint8_t unsigned_duty_percent_buffer; //!< 上位アプリからCMD指示されたPWMの符号なしduty (PWMタイマがゼロの時にunsigned_duty_percentへ反映)
-    MTQ_SEIREN_POLARITY polarity_buffer;  //!< 上位アプリからCMD指示されたMTQ駆動極性
-    struct
-    {
-      uint8_t unsigned_duty_percent;  //!< PWMの符号なしduty
-      ObcTime last_set_time;          //!< 前回実行時のOBC時刻
-      uint32_t internal_timer_ms;     //!< PWMタイマカウンタ．pwm_period_ms_まで増加したら0に戻る
-    } pwm_info;
   } driver;
   MTQ_SEIREN_Info info;
 } MTQ_SEIREN_Driver;
-
-// (現状では) PWM制御周期が遅く，消磁のためのOn/Off処理とこの値の大小に応じた処理分岐が必要な状況なので，外部参照可能にする
-extern const uint32_t* MTQ_SEIREN_pwm_period_ms;
 
 // 基本関数
 
@@ -75,35 +61,6 @@ extern const uint32_t* MTQ_SEIREN_pwm_period_ms;
  * @return GPIO_ERR_CODEに準じる
  */
 GPIO_ERR_CODE MTQ_SEIREN_init(MTQ_SEIREN_Driver* mtq_seiren_driver, uint8_t ch_gpio_positive, uint8_t ch_gpio_negative, float max_mag_moment);
-
-/**
- * @brief  MTQ_SEIREN duty，極性設定
- *
- *         MTQ_SEIREN_Driver構造体のポインタを渡し，PWMのdutyを設定する
- * @param  *mtq_seiren        : 初期化するMTQ_SEIREN_Driver構造体へのポインタ
- * @param  signed_duty        : 設定する符号つきduty値[％]
- * @return DS_CMD_ERR_CODEに準じる
- */
-DS_CMD_ERR_CODE MTQ_SEIREN_set_pwm_signed_duty(MTQ_SEIREN_Driver* mtq_seiren_driver, int8_t pwm_signed_duty_percent);
-
-/**
- * @brief  MTQ_SEIREN PWM周期設定
- *
- *         MTQ_SEIREN_Driver構造体のポインタを渡し，PWMのdutyを設定する
- * @param  *mtq_seiren   : 初期化するMTQ_SEIREN_Driver構造体へのポインタ
- * @param  pwm_period_us : 設定するpwm周期
- * @return DS_CMD_ERR_CODEに準じる
- */
-DS_CMD_ERR_CODE MTQ_SEIREN_set_pwm_period_ms(uint32_t pwm_period_ms);
-
-/**
- * @brief  MTQ_SEIREN PWM出力
- *
- *         MTQ_SEIREN_Driver構造体のポインタを渡し，PWMのdutyを設定する
- * @param  *mtq_seiren   : MTQ_SEIREN_Driver構造体へのポインタ
- * @return DS_CMD_ERR_CODEに準じる
- */
-DS_CMD_ERR_CODE MTQ_SEIREN_output_pwm(MTQ_SEIREN_Driver* mtq_seiren_driver);
 
 /**
  * @brief  磁気モーメント方向ベクトル設定関数
