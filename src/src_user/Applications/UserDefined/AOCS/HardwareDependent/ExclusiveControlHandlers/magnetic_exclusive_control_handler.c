359ce5cc92df54da0546f81d671782cc50689321
Toshihiro Suzuki <suzuki@arkedgespace.com>
2023-11-08 12:07:53 +0900

    [MagCtrl] Add file for magnetic exclusive control handler for #16

---
+++ src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.c
@@ -0,0 +1,4 @@
+/**
+* @file   magnetic_exclusive_control_handler.c
+* @brief  MTQ-磁気センサ排他制御ハンドラー
+*/
---
+++ src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.h
@@ -0,0 +1,15 @@
+/**
+* @file   magnetic_exclusive_control_handler.h
+* @brief  MTQ-磁気センサ排他制御ハンドラー
+*/
+
+#ifndef MAGNETIC_EXCLUSIVE_CONTROL_HANDLER_H_
+#define MAGNETIC_EXCLUSIVE_CONTROL_HANDLER_H_
+
+#include <stdint.h>
+#include <src_core/System/ApplicationManager/app_info.h>
+#include <src_core/TlmCmd/common_cmd_packet.h>
+#include <src_core/System/TimeManager/time_manager.h>
+#include <src_user/Library/physical_constants.h>
+
+#endif
