bf81e8a3d0ffee20e13846708830def499c85ade
Toshihiro Suzuki <suzuki@arkedgespace.com>
2023-11-08 15:10:17 +0900

    [MagCtrl] Implement magnecit exclusive control handler for #16

--- src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.c
+++ src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.c
@@ -1,4 +1,78 @@
+#pragma section REPRO
 /**
 * @file   magnetic_exclusive_control_handler.c
 * @brief  MTQ-磁気センサ排他制御ハンドラー
 */
+
+#include "magnetic_exclusive_control_handler.h"
+
+static MagneticExclusiveControlHandler        magnetic_exclusive_control_handler_;
+const  MagneticExclusiveControlHandler* const magnetic_exclusive_control_handler = &magnetic_exclusive_control_handler_;
+
+static void APP_MECH_init_(void);
+static void APP_MECH_exec_(void);
+
+static void APP_MECH_update_timer_(void);
+static void APP_MECH_update_state_(void);
+
+AppInfo APP_MECH_create_app(void)
+{
+  return AI_create_app_info("MagneticExclusiveControlHandler", APP_MECH_init_, APP_MECH_exec_);
+}
+
+void APP_MECH_init_(void)
+{
+  magnetic_exclusive_control_handler_.previous_obc_time = TMGR_get_master_clock();
+  magnetic_exclusive_control_handler_.current_state     = APP_MECH_STATE_STANDBY;
+  magnetic_exclusive_control_handler_.state_timer_ms    = 0;
+  magnetic_exclusive_control_handler_.observe_duration_ms = 100;
+  magnetic_exclusive_control_handler_.control_duration_ms = 800;
+  magnetic_exclusive_control_handler_.standby_duration_ms = 100;
+}
+
+void APP_MECH_exec_(void)
+{
+  APP_MECH_update_timer_();
+  APP_MECH_update_state_();
+}
+
+
+void APP_MECH_update_timer_(void)
+{
+  ObcTime current_obc_time = TMGR_get_master_clock();
+  uint32_t step_time_ms = OBCT_diff_in_msec(&(magnetic_exclusive_control_handler_.previous_obc_time), &current_obc_time);
+  magnetic_exclusive_control_handler_.previous_obc_time = current_obc_time;
+
+  magnetic_exclusive_control_handler_.state_timer_ms += step_time_ms;
+}
+
+void APP_MECH_update_state_(void)
+{
+  switch (magnetic_exclusive_control_handler_.current_state)
+  {
+  case APP_MECH_STATE_OBSERVE:
+    if (magnetic_exclusive_control_handler_.state_timer_ms >= magnetic_exclusive_control_handler_.observe_duration_ms)
+    {
+      magnetic_exclusive_control_handler_.state_timer_ms = 0;
+      magnetic_exclusive_control_handler_.current_state = APP_MECH_STATE_CONTROL;
+    }
+    break;
+  case APP_MECH_STATE_CONTROL:
+    if (magnetic_exclusive_control_handler_.state_timer_ms >= magnetic_exclusive_control_handler_.control_duration_ms)
+    {
+      magnetic_exclusive_control_handler_.state_timer_ms = 0;
+      magnetic_exclusive_control_handler_.current_state = APP_MECH_STATE_STANDBY;
+    }
+    break;
+  case APP_MECH_STATE_STANDBY:
+    if (magnetic_exclusive_control_handler_.state_timer_ms >= magnetic_exclusive_control_handler_.standby_duration_ms)
+    {
+      magnetic_exclusive_control_handler_.state_timer_ms = 0;
+      magnetic_exclusive_control_handler_.current_state = APP_MECH_STATE_OBSERVE;
+    }
+    break;
+  default:
+    // NOT REACHED
+    break;
+  }
+}
--- src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.h
+++ src/src_user/Applications/UserDefined/AOCS/HardwareDependent/ExclusiveControlHandlers/magnetic_exclusive_control_handler.h
@@ -12,4 +12,35 @@
 #include <src_core/System/TimeManager/time_manager.h>
 #include <src_user/Library/physical_constants.h>
 
+/**
+ * @enum  APP_MECH_STATE
+ * @brief 排他制御の現在の状態
+ * @note  状態は観測 --> 制御 --> スタンバイ --> 観測 --> ... と切り替わる
+ * @note  uint8_tを想定
+*/
+typedef enum
+{
+  APP_MECH_STATE_OBSERVE = 0, //!< 磁気センサで磁場観測する
+  APP_MECH_STATE_CONTROL,     //!< MTQ出力によって姿勢制御する
+  APP_MECH_STATE_STANDBY      //!< 磁気センサの読みからMTQ出力由来のノイズの影響が抜けるのを待つ
+} APP_MECH_STATE;
+
+/**
+ * @struct MagneticExclusiveControlHandler
+ * @brief  排他制御に必要な情報を管理する構造体
+*/
+typedef struct
+{
+  ObcTime  previous_obc_time;   //!< 前回のアプリ実行時刻
+  APP_MECH_STATE current_state; //!< 現在の排他制御状態
+  uint32_t state_timer_ms;      //!< 現在の排他制御状態に入ってから経過した時間
+  uint32_t observe_duration_ms; //!< 磁気センサ観測状態の持続時間
+  uint32_t control_duration_ms; //!< MTQ出力状態の持続時間
+  uint32_t standby_duration_ms; //!< スタンバイ状態の持続時間
+} MagneticExclusiveControlHandler;
+
+extern const MagneticExclusiveControlHandler* magnetic_exclusive_control_handler;
+
+AppInfo APP_MECH_create_app(void);
+
 #endif
