43b02969ee6ab4489f8e9c559d4648e0aa46487f
Hiro-0110 <okada.hirotaka.0110@gmail.com>
2024-02-19 17:58:54 +0900

    resolve code format error

--- src/src_user/Applications/UserDefined/AOCS/TargetAttitude/target_attitude_from_orbit.c
+++ src/src_user/Applications/UserDefined/AOCS/TargetAttitude/target_attitude_from_orbit.c
@@ -194,7 +194,7 @@ static void APP_TAFO_calc_target_direction_vec_eci_(float target_direction_vec_e
       VECTOR3_copy(target_direction_vec_eci_normalized, sat_vel_vec_est_eci_normalized);
       break;
     }
-  case APP_TAFO_TARGET_DIRECTION_SAT_RELATIVE_VELOCITY:
+  case APP_TAFO_TARGET_DIRECITON_SAT_RELATIVE_VELOCITY:
     {
       double dcm_ecef_to_eci[PHYSICAL_CONST_THREE_DIM][PHYSICAL_CONST_THREE_DIM];
       double sat_vel_est_ecef_m_s[PHYSICAL_CONST_THREE_DIM];
@@ -217,22 +217,22 @@ static void APP_TAFO_calc_target_direction_vec_eci_(float target_direction_vec_e
       VECTOR3_normalize(sat_vel_rel_vec_est_ecef_m_s_normalized, float_sat_rel_vel_est_ecef_m_s);
       VECTOR3_copy(target_direction_vec_eci_normalized, sat_vel_rel_vec_est_ecef_m_s_normalized);
       break;
-  }
-  case APP_TAFO_TARGET_DIRECITON_EARTH_CENTER:
-  {
-    float float_sat_pos_est_eci_m[PHYSICAL_CONST_THREE_DIM];
-    float sat_pos_vec_est_eci_normalized[PHYSICAL_CONST_THREE_DIM];
-    float earth_vec_est_eci_normalized[PHYSICAL_CONST_THREE_DIM];
-
-    // [TODO_L] Vector libにdouble -> float, float -> doubleキャスト関数を実装する
-    for (int idx = 0; idx < PHYSICAL_CONST_THREE_DIM; idx++)
-    {
-      float_sat_pos_est_eci_m[idx] = (float)aocs_manager->sat_pos_est_eci_m[idx];
     }
-    VECTOR3_normalize(sat_pos_vec_est_eci_normalized, float_sat_pos_est_eci_m);
-    VECTOR3_scalar_product(earth_vec_est_eci_normalized, -1.0f, sat_pos_vec_est_eci_normalized);
-    VECTOR3_copy(target_direction_vec_eci_normalized, earth_vec_est_eci_normalized);
-    break;
+  case APP_TAFO_TARGET_DIRECITON_EARTH_CENTER:
+    {
+      float float_sat_pos_est_eci_m[PHYSICAL_CONST_THREE_DIM];
+      float sat_pos_vec_est_eci_normalized[PHYSICAL_CONST_THREE_DIM];
+      float earth_vec_est_eci_normalized[PHYSICAL_CONST_THREE_DIM];
+
+      // [TODO_L] Vector libにdouble -> float, float -> doubleキャスト関数を実装する
+      for (int idx = 0; idx < PHYSICAL_CONST_THREE_DIM; idx++)
+      {
+        float_sat_pos_est_eci_m[idx] = (float)aocs_manager->sat_pos_est_eci_m[idx];
+      }
+      VECTOR3_normalize(sat_pos_vec_est_eci_normalized, float_sat_pos_est_eci_m);
+      VECTOR3_scalar_product(earth_vec_est_eci_normalized, -1.0f, sat_pos_vec_est_eci_normalized);
+      VECTOR3_copy(target_direction_vec_eci_normalized, earth_vec_est_eci_normalized);
+      break;
   }
   case APP_TAFO_TARGET_DIRECITON_EARTH_SURFACE:
     {
--- src/src_user/Applications/UserDefined/AOCS/TargetAttitude/target_attitude_from_orbit.h
+++ src/src_user/Applications/UserDefined/AOCS/TargetAttitude/target_attitude_from_orbit.h
@@ -1,7 +1,7 @@
 /**
- * @file   target_attitude_from_orbit.h
- * @brief  推定軌道から目標姿勢を計算するアプリ
- */
+* @file   target_attitude_from_orbit.h
+* @brief  推定軌道から目標姿勢を計算するアプリ
+*/
 #ifndef TARGET_ATTITUDE_FROM_ORBIT_H_
 #define TARGET_ATTITUDE_FROM_ORBIT_H_
 
@@ -18,12 +18,12 @@
  */
 typedef enum
 {
-  APP_TAFO_TARGET_DIRECITON_SUN = 0,               //!< 太陽方向
-  APP_TAFO_TARGET_DIRECITON_SAT_VELOCITY,          //!< 衛星速度方向
-  APP_TAFO_TARGET_DIRECTION_SAT_RELATIVE_VELOCITY, //!< 衛星相対速度方向
-  APP_TAFO_TARGET_DIRECITON_EARTH_CENTER,          //!< 地心方向
-  APP_TAFO_TARGET_DIRECITON_EARTH_SURFACE,         //!< 地表点
-  APP_TAFO_TARGET_DIRECITON_ORBIT_NORMAL,          //!< 軌道面外方向
+  APP_TAFO_TARGET_DIRECITON_SUN = 0,                  //!< 太陽方向
+  APP_TAFO_TARGET_DIRECITON_SAT_VELOCITY,             //!< 衛星速度方向
+  APP_TAFO_TARGET_DIRECITON_SAT_RELATIVE_VELOCITY,    //!< 衛星相対速度方向
+  APP_TAFO_TARGET_DIRECITON_EARTH_CENTER,             //!< 地心方向
+  APP_TAFO_TARGET_DIRECITON_EARTH_SURFACE,            //!< 地表点
+  APP_TAFO_TARGET_DIRECITON_ORBIT_NORMAL,             //!< 軌道面外方向
   APP_TAFO_TARGET_DIRECITON_MAX
 } APP_TAFO_TARGET_DIRECITON;
 
@@ -33,18 +33,18 @@ typedef enum
  */
 typedef struct
 {
-  Quaternion quaternion_target_i2t;                        //!< 計算された目標Quaternion
-  uint8_t is_enabled;                                      //!< 目標Quaternionをaocs_managerにセットするか否か
-  APP_TAFO_TARGET_DIRECITON main_target_direction;         //!< vec_to_main_target_bodyの目標方向
-  APP_TAFO_TARGET_DIRECITON sub_target_direction;          //!< vec_to_sub_target_bodyの目標方向
-  float vec_to_main_target_body[PHYSICAL_CONST_THREE_DIM]; //!< main_target_directionに向ける機体座標系ベクトル
-  float vec_to_sub_target_body[PHYSICAL_CONST_THREE_DIM];  //!< 姿勢を一意に決定するために指定する機体座標系ベクトル
-  MATRIX33_ROTATION_AXIS offset_angle_axis;                //!< オフセット角を与える軸
-  float offset_angle_rad;                                  //!< オフセット角, rad
-  float target_lla_rad_m[PHYSICAL_CONST_THREE_DIM];        //!< 目標地表点の緯度・経度・高度, rad+rad+m
+  Quaternion quaternion_target_i2t;                         //!< 計算された目標Quaternion
+  uint8_t is_enabled;                                       //!< 目標Quaternionをaocs_managerにセットするか否か
+  APP_TAFO_TARGET_DIRECITON main_target_direction;          //!< vec_to_main_target_bodyの目標方向
+  APP_TAFO_TARGET_DIRECITON sub_target_direction;           //!< vec_to_sub_target_bodyの目標方向
+  float vec_to_main_target_body[PHYSICAL_CONST_THREE_DIM];  //!< main_target_directionに向ける機体座標系ベクトル
+  float vec_to_sub_target_body[PHYSICAL_CONST_THREE_DIM];   //!< 姿勢を一意に決定するために指定する機体座標系ベクトル
+  MATRIX33_ROTATION_AXIS offset_angle_axis;                   //!< オフセット角を与える軸
+  float offset_angle_rad;                                   //!< オフセット角, rad
+  float target_lla_rad_m[PHYSICAL_CONST_THREE_DIM];         //!< 目標地表点の緯度・経度・高度, rad+rad+m
 } TargetAttitudeFromOrbit;
 
-extern const TargetAttitudeFromOrbit *const target_attitude_from_orbit;
+extern const TargetAttitudeFromOrbit* const target_attitude_from_orbit;
 
 /**
  * @brief  目標姿勢計算アプリ生成関数
@@ -53,10 +53,10 @@ extern const TargetAttitudeFromOrbit *const target_attitude_from_orbit;
  */
 AppInfo APP_TAFO_create_app(void);
 
-CCP_CmdRet Cmd_APP_TAFO_SET_MAIN_TARGET_DIRECTION(const CommonCmdPacket *packet);
-CCP_CmdRet Cmd_APP_TAFO_SET_SUB_TARGET_DIRECTION(const CommonCmdPacket *packet);
-CCP_CmdRet Cmd_APP_TAFO_SET_OFFSET_ANGLE_AND_AXIS(const CommonCmdPacket *packet);
-CCP_CmdRet Cmd_APP_TAFO_SET_TARGET_LLA(const CommonCmdPacket *packet);
-CCP_CmdRet Cmd_APP_TAFO_ENABLE(const CommonCmdPacket *packet);
+CCP_CmdRet Cmd_APP_TAFO_SET_MAIN_TARGET_DIRECTION(const CommonCmdPacket* packet);
+CCP_CmdRet Cmd_APP_TAFO_SET_SUB_TARGET_DIRECTION(const CommonCmdPacket* packet);
+CCP_CmdRet Cmd_APP_TAFO_SET_OFFSET_ANGLE_AND_AXIS(const CommonCmdPacket* packet);
+CCP_CmdRet Cmd_APP_TAFO_SET_TARGET_LLA(const CommonCmdPacket* packet);
+CCP_CmdRet Cmd_APP_TAFO_ENABLE(const CommonCmdPacket* packet);
 
 #endif
