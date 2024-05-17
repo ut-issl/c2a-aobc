e0bc49220fe608fd253c618d8b301a7a6a238ddf
ogoogo <ogosae.1018173@docomo.ne.jp>
2024-05-18 00:40:24 +0900

    命名修正

--- src/src_user/Settings/SatelliteParameters/stim210_parameters.h
+++ src/src_user/Settings/SatelliteParameters/stim210_parameters.h
@@ -32,6 +32,6 @@ extern const float STIM210_PARAMETERS_gyro_spike_reject_threshold_rad_s[PHYSICAL
 extern const float STIM210_PARAMETERS_gyro_spike_amplitude_limit_to_accept_as_step_rad_s[PHYSICAL_CONST_THREE_DIM];   //!< Amplitude limit to accept as step input [rad/s]
 
 // Magnetic sensor bias calibration
-extern const float STIM210_PARAMETERS_mag_bias_compo[PHYSICAL_CONST_THREE_DIM]; //!< Bias @ component frame X axis
+extern const float STIM210_mag_bias_rm3100_ext_compo_nT[PHYSICAL_CONST_THREE_DIM]; //!< Bias @ component frame X axis
 
 #endif // STIM210_PARAMETERS_H_
