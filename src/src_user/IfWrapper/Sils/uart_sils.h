2e0c6b604f51f4d4504e0339c028c91324a8a105
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2024-12-12 02:15:42 +0900

    Add s2e namespace

--- src/src_user/IfWrapper/Sils/uart_sils.h
+++ src/src_user/IfWrapper/Sils/uart_sils.h
@@ -7,8 +7,8 @@
 #define UART_SILS_H_
 
 
-int OBC_C2A_SendFromObc(int port_id, unsigned char* buffer, int offset, int count);
-int OBC_C2A_ReceivedByObc(int port_id, unsigned char* buffer, int offset, int count);
+int s2e::components::OBC_C2A_SendFromObc(int port_id, unsigned char* buffer, int offset, int count);
+int s2e::components::OBC_C2A_ReceivedByObc(int port_id, unsigned char* buffer, int offset, int count);
 
 int UART_init(void* my_uart_v);
 int UART_rx(void* my_uart_v, void* data_v, int buffer_size);
